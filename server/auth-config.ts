41931ff59a0d58b73237d8e156206aaf86fa083e
MhzZZZZZ322322 <vpxxp1@gmail.com>
2025-09-19 21:58:23 +0300

    Update auth-config.ts

--- server/auth-config.ts
+++ server/auth-config.ts
@@ -1,5 +1,19 @@
 // server/auth-config.ts
 
+// Definim tipurile pentru request, response și next pentru claritate
+// Poți instala @types/express (`npm install --save-dev @types/express`) pentru tipuri complete
+interface Request {
+  // Aici poți adăuga proprietăți specifice dacă este necesar
+}
+
+interface Response {
+  status: (code: number) => {
+    send: (message: string) => void;
+  };
+}
+
+type NextFunction = () => void;
+
 /**
  * Middleware pentru a verifica credentialele de administrator.
  *
@@ -7,13 +21,14 @@
  * De exemplu, poți verifica un username și o parolă trimise în header-ul cererii
  * cu valorile stocate în variabile de mediu.
  */
-export const verifyAdminCredentials = (req, res, next) => {
+export const verifyAdminCredentials = (req: Request, res: Response, next: NextFunction) => {
   console.log("ATENȚIE: Verificarea de admin este un placeholder. Accesul este permis implicit.");
 
   // TODO: Implementează aici logica ta de autentificare.
   // Exemplu:
-  // const { username, password } = req.body;
-  // if (username === process.env.ADMIN_USER && password === process.env.ADMIN_PASS) {
+  // const { authorization } = req.headers;
+  // const token = authorization?.split(' ')[1]; // Bearer TOKEN
+  // if (token === process.env.ADMIN_SECRET_TOKEN) {
   //   next(); // Credentiale corecte, continuă
   // } else {
   //   res.status(401).send('Unauthorized'); // Credentiale greșite
